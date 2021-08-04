0847c70a1176873a1c05a2792340823a56d77c26
Кусь Пчелов <kycb42148@gmail.com>
2021-08-04 19:18:43 +0300

    Added 'level' command hint in inventory

--- bot.js
+++ bot.js
@@ -123,7 +123,7 @@ client.on('message', message => {
         if(text.match(commands.inventory)){
             message.channel.send(new Discord.MessageEmbed().setColor("#ffae00").setAuthor("Inventory:")
                 .addField(`Coins:`, `${userdata.money}${e.vc}`, true)
-                .addField(`Level:`, `${userdata.lvl}${e.up}`, true)
+                .addField(`Level:`, `${userdata.lvl}${e.up} ||(команда s!lvl)||`, true)
                 .setTimestamp().setFooter('By Tomoko and Kycb42148', 'https://avatars.githubusercontent.com/u/34296702?v=4'));
         }
         fs.writeFileSync(__dirname + `/data/users/${userId}.json`, JSON.stringify(userdata));
