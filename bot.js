1f1c8fd590ae0b87f7ab1ef1793e6ff5bac1d6ac
Кусь Пчелов <kycb42148@gmail.com>
2021-08-06 16:35:49 +0300

    Added 'fishing' command regex

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ const commands = {
     inventory: /^s!(i|inv|inventory|my|me|profile)$/i,
     level: /^s!(l|lvl|level)$/i,
     work: /^s!(w|work)$/i,
+    fish: /^s!(f|fish|fishing)$/i,
     casino: /^s!(c|casino) (\d{1,3})$/i,
     transfer: /^s!(t|pay)( <@![0-9]{16,20}>)? (\d{1,3})$/i,
 };
