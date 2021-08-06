3f00feaf18da16713ff4d87557b7e9cab2d2ea98
Кусь Пчелов <kycb42148@gmail.com>
2021-08-06 16:55:34 +0300

    Changed userdata format

--- bot.js
+++ bot.js
@@ -182,7 +182,7 @@ function getUserdata(userId){
         fs.accessSync(`${__dirname}\\data\\users\\${userId}.json`);
         return JSON.parse(fs.readFileSync(`${__dirname}\\data\\users\\${userId}.json`));
     } catch (error){
-        let userdata = { money: 20, xp: 0, lvl: 0, lastuse: { work: 0}, lastcomplete: { work: 0 }, task: "idle"};
+        let userdata = { money: 20, xp: 0, lvl: 0, lastuse: { work: 0, fish: 0 }, lastcomplete: { work: 0, fish: 0 }, task: "idle", items: { fish: 0 }};
         fs.writeFileSync(__dirname + `\\data\\users\\${userId}.json`, JSON.stringify(userdata));
         return userdata;
     }
