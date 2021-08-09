ba47229fcfc02a383f83b0dee3677ea0a68cc894
Кусь Пчелов <kycb42148@gmail.com>
2021-08-09 15:47:34 +0300

    Add 'give' command regex

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ const commands = {
     fish: /^s!(f|fish|fishing)$/i,
     casino: /^s!(c|casino) (\d{1,3})$/i,
     transfer: /^s!(t|pay)( <@(!)?[0-9]{16,20}>)? (\d{1,3})$/i,
+    give: /^s!(g|gift|give)( <@(!)?[0-9]{16,20}>)? (fish)$/
 };
 
 const cooldown = (is_debug)? { work: 6000, fish: 6000 } : { work: 180000, fish: 240000 };
